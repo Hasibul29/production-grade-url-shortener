0e2e7843de889d79da8559daf648520b127add7e
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2023-04-18 16:31:35 +0600

    Fix invalid Url bug

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,24 +1,33 @@
 import { FastifyPluginAsync } from 'fastify';
+import isUrl = require('is-url');
 
 interface RequestParam {
     short: string;
 }
 
-const arr: string[] = [];
+const urlStorage: string[] = [];
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     fastify.post('/', async (request, reply) => {
-        const key: number = arr.length;
-        if (!request.body) return reply.code(400).send('Invalid url');
-        arr[key] = request.body as string;
+        const givenUrl: string = request.body as string;
+        const isValid = isUrl(givenUrl);
+        const key: number = urlStorage.length;
+
+        if (!givenUrl || !isValid) {
+            return reply.code(400).send('Invalid url');
+        }
+
+        urlStorage[key] = givenUrl;
         return `${request.protocol}://${request.hostname}${request.url}${key}`;
     });
 
     fastify.get('/:short', async (request, reply) => {
         const id = request.params as RequestParam;
-        if (Number(id.short) >= arr.length) reply.code(400).send('Invalid url');
+        if (Number(id.short) >= urlStorage.length) {
+            reply.code(400).send('Invalid url');
+        }
         return {
-            'long-url': arr[Number(id.short)],
+            'long-url': urlStorage[Number(id.short)],
             'short-url ': `${request.protocol}://${request.hostname}/${id.short}`,
         };
     });
