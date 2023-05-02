a292b355952fd6ab2d3f40c6e52f45fc25abd388
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2023-05-02 19:10:14 +0600

    feature:users can login to their account

--- src/routes/login/index.ts
+++ src/routes/login/index.ts
@@ -1,8 +1,45 @@
+import * as bcrypt from 'bcrypt';
 import { FastifyPluginAsync } from 'fastify';
+
+interface LoginRequest {
+    email: string;
+    password: string;
+}
 
 const login: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     fastify.get('/', async function (request, reply) {
         return 'login Page';
+    });
+    fastify.post('/', async function (request, reply) {
+        const { email, password } = request.body as LoginRequest;
+
+        const client = await fastify.pg.connect();
+
+        try {
+            const userData = await client.query(
+                'Select * From Users Where email = $1',
+                [email]
+            );
+            if (!userData) {
+                return "User Doesn't Exist";
+            } else {
+                const hashedPassword = userData.rows[0].password_hash;
+                const isPasswordCorrect = await bcrypt.compare(
+                    password,
+                    hashedPassword
+                );
+                if (isPasswordCorrect) {
+                    return 'login Success';
+                } else {
+                    return 'Wrong Password';
+                }
+            }
+        } catch (err) {
+            console.log('There Is an error', err);
+        } finally {
+            client.release();
+        }
+        return request.body;
     });
 };
 
