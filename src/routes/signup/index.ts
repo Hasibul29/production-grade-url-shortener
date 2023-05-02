b91fd92e6486ae2e475a65824b7c25dae25beb53
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2023-05-02 17:37:02 +0600

    feature: users are able to create an account.

--- src/routes/signup/index.ts
+++ src/routes/signup/index.ts
@@ -1,7 +1,34 @@
+import * as bcrypt from 'bcrypt';
 import { FastifyPluginAsync } from 'fastify';
+
+interface UserInfo {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
 const signup: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     fastify.get('/', async function (request, reply) {
         return 'signup';
+    });
+    fastify.post('/', async function (request, reply) {
+        const { email, password, confirmPassword } = request.body as UserInfo;
+
+        if (password != confirmPassword) {
+            return "Password Didn't Match";
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const client = await fastify.pg.connect();
+
+        await client.query(
+            'INSERT INTO Users(email,password_hash,password_salt) VALUES ($1, $2, $3)',
+            [email, hashedPassword, salt]
+        );
+
+        return 'Registration Complete';
     });
 };
 
