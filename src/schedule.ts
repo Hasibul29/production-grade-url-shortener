661ec350777c464003b250c0dec9bc4f484f1369
Sheikh Md. Hasibul Hasan <skhasibul.c17@gmail.com>
2023-05-10 18:40:26 +0600

    add connection to queue and worker

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,10 +1,17 @@
+import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { FastifyAdapter } from '@bull-board/fastify';
 import { Queue, Worker } from 'bullmq';
+import * as dotenv from 'dotenv';
 import { FastifyPluginAsync } from 'fastify';
-const { createBullBoard } = require('@bull-board/api');
-const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
-const { FastifyAdapter } = require('@bull-board/fastify');
 
-export const myQueue = new Queue('url-expire');
+dotenv.config();
+export const myQueue = new Queue('url-expire', {
+    connection: {
+        host: 'localhost',
+        port: 6379,
+    },
+});
 
 export const schedule: FastifyPluginAsync = async (fastify) => {
     const serverAdapter = new FastifyAdapter();
@@ -12,10 +19,17 @@ export const schedule: FastifyPluginAsync = async (fastify) => {
         queues: [new BullMQAdapter(myQueue)],
         serverAdapter,
     });
-
     serverAdapter.setBasePath('/ui');
-    fastify.register(serverAdapter.registerPlugin(), { prefix: '/ui' });
-    const worker = new Worker('url-expire', async (job) => {});
+    fastify.register(serverAdapter.registerPlugin(), {
+        prefix: '/ui',
+        basePath: 'ui',
+    });
+    const worker = new Worker('url-expire', async (job) => {}, {
+        connection: {
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT),
+        },
+    });
     worker.on('completed', async (job) => {
         const client = await fastify.pg.connect();
         try {
